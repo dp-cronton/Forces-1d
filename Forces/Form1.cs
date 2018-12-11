3cb1655fffdc9f094129a15445764c2900697e34
David Petticrew <David.Petticrew@riversidecollege.ac.uk>
2018-12-11 10:29:23 +0000

    Working component calculations

--- Forces/Form1.cs
+++ Forces/Form1.cs
@@ -16,5 +16,52 @@ namespace Forces
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double f = double.Parse(textBox1.Text);
+                double a = double.Parse(textBox2.Text);
+                double fx = f * cos(a);
+                double fy = f * sin(a);
+                label1.Text = "Fx = " + fx + " N";
+                label2.Text = "Fy = " + fy + " N";            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+        }
+
+        double sin(double a)
+        {
+            return Math.Sin(a / 180.0 * Math.PI);
+        }
+
+        double cos(double a)
+        {
+            return Math.Cos(a / 180.0 * Math.PI);
+        }
+
+        double inverseSin(double a)
+        {
+            return Math.Asin(a)*180.0/Math.PI;
+        }
+
+        double inverseCos(double a)
+        {
+            return Math.Acos(a) * 180.0 / Math.PI;
+        }
+
+        double tan(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Tan(rad);
+        }
+
+        double inverseTan(double a)
+        {
+            return Math.Atan(a) * 180.0 / Math.PI;
+        }
     }
 }
